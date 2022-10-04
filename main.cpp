82a8c6d46300d5ba62aee3f795e19f8422ac77ab
Sean Gilleece <47053602+Gilleece@users.noreply.github.com>
2022-10-04 11:50:23 +0100

    Added initial ball movement and frame timing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include "raylib.h"
-
+//25:27
 int main()
 {
     InitWindow(800, 600, "Ultimate Pong");
@@ -8,16 +8,25 @@ int main()
     //Object Sizes
     int paddleHeight = 100;
     int paddleWidth = 10;
-    int ballSize = 10;
+    float ballSize = 5;
     
     //Object Positions
+    float ballX = GetScreenWidth() / 2.0f;
+    float ballY = GetScreenHeight() / 2.0f;
+
+    //Ball Speed
+    float ballSpeedX = 300;
+    float ballSpeedY = 300;
     
     while (!WindowShouldClose())
     {
+        ballX += ballSpeedX * GetFrameTime();
+        ballY += ballSpeedY * GetFrameTime();
+
         BeginDrawing();
             ClearBackground(BLACK);
 
-            DrawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2, ballSize, WHITE);
+            DrawCircle((int) ballX, (int) ballY, ballSize, WHITE);
             DrawRectangle(50, (GetScreenHeight() / 2) - (paddleHeight / 2), paddleWidth, paddleHeight, WHITE);
             DrawRectangle(GetScreenWidth() - 50, (GetScreenHeight() / 2) - (paddleHeight / 2), paddleWidth, paddleHeight, WHITE);
 
